21e70719f019caf4ea32b20d37901936680ae3fe
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-06-12 12:17:52 -0300

    feat: add validation on create user Controller

--- src/controllers/createUser.ts
+++ src/controllers/createUser.ts
@@ -1,36 +1,49 @@
-import { PrismaUserRepository, type CreateUserDTO } from "../repository/prisma/createUser.js";
-import type { Request, Response } from "express";
+import { PrismaUserRepository } from "../repository/prisma/createUser.js";
+import type { Request } from "express";
 import { CreateUserUseCase } from "../use-cases/users/createUser.js";
+import validator from "validator";
+import { badRequest, created, serverError } from "./helpers.js";
+
 export class CreateUserController {
   async create(httpRequest: Request) {
     try {
       const params = httpRequest.body;
-      const requiredFields = ["name", "email", "password"];
+      const requiredFields = ["name", "email", "password", "confirmPassword"];
+      const { confirmPassword, ...dataUser } = params;
 
       for (const field of requiredFields) {
-        if (!params[field] || params[field].trim().lenght === 0) {
-          return {
-            statusCode: 400,
-            body: {
-              errorMessage: `Missing field value ${params}`,
-            },
-          };
+        if (!params[field] || params[field].trim().length === 0) {
+          return badRequest({ message: `Missing field value ${field}` });
         }
       }
-      const createUserUseCase = new CreateUserUseCase(new PrismaUserRepository());
-      const createdUser = await createUserUseCase.create(params);
+      const emailIsValid = validator.isEmail(params.email);
 
-      return {
-        statusCode: 201,
-        body: createdUser,
-      };
+      if (!emailIsValid) {
+        return badRequest({
+          message: "Invalid e-mail. Please provider a valid one",
+        });
+      }
+      const passwordIsValid = params.password.length < 6;
+      if (passwordIsValid) {
+        return badRequest({
+          message: "Password must be at least 6 characters",
+        });
+      }
+      if (params.password != params.confirmPassword) {
+        return badRequest({
+          message: "Password must be at least 6 characters",
+        });
+      }
+
+      const createUserUseCase = new CreateUserUseCase(
+        new PrismaUserRepository(),
+      );
+      const createdUser = await createUserUseCase.create(dataUser);
+
+      return created(createdUser);
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: {
-          errorMessage: "Iternal server error",
-        },
-      };
+      console.log(error);
+      return serverError();
     }
   }
 }
---
+++ src/controllers/helpers.ts
@@ -0,0 +1,19 @@
+export const badRequest = (body: Record<string, unknown>) => {
+  return {
+    statusCode: 400,
+    body,
+  };
+};
+
+export const created = (body: Record<string, unknown>) => {
+  return {
+    statusCode: 201,
+    body,
+  };
+};
+
+export const serverError = () => {
+  return {
+    message: "Internal Server Error",
+  };
+};
