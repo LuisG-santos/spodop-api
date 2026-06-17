7b3af64ff67e11ec5e98bac4257482814dc2dc89
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-06-17 17:27:43 -0300

    refactor: improve code formatting and consistency in GetUserByIdController

--- src/controllers/user/getUserById.ts
+++ src/controllers/user/getUserById.ts
@@ -2,26 +2,25 @@ import type { Request, Response } from "express";
 import { GetUserByIdUseCase } from "../../use-cases/users/getuserById.js";
 
 export class GetUserByIdController {
-  private getUserByIdUseCase: GetUserByIdUseCase
-  constructor(getUserById: GetUserByIdUseCase){
-    this.getUserByIdUseCase = getUserById
-
+  private getUserByIdUseCase: GetUserByIdUseCase;
+  constructor(getUserById: GetUserByIdUseCase) {
+    this.getUserByIdUseCase = getUserById;
   }
-  async getUser(request: Request<{ id: string }>, res: Response) {
+  async getUser(req: Request<{ id: string }>, res: Response) {
     try {
-      const { id } = request.params;
+      const { id } = req.params;
       if (!id) {
-        return res.status(400).json({message: "id param is missing"});
+        return res.status(400).json({ message: "id param is missing" });
       }
 
       const getUser = await this.getUserByIdUseCase.getUserById(id);
       if (!getUser) {
-        return res.status(404).json({message: "user not found"});
+        return res.status(404).json({ message: "user not found" });
       }
       res.status(200).json(getUser);
     } catch (error) {
       console.log(error);
-      res.status(500).json({message: "internal server error"});
+      return res.status(500).json({ message: "internal server error" });
     }
   }
 }
