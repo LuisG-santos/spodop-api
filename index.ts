8a9a3efdafd7e39118fa365a5308cd572ee33266
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-06-23 23:21:25 -0300

    refactor: reorder imports in index.ts for improved organization

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./src/routes/user/user.routes.js";
 import authRoutes from "./src/routes/auth/auth.routes.js";
-const app = express();
+import "dotenv/config";
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
