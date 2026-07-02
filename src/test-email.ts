a0afef5761a76ce336af1ef6d86de27cdb2c407b
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-07-02 01:13:05 -0300

    feat: remove test email script for reset password

--- src/test-email.ts
+++
@@ -1,5 +0,0 @@
-import { sendResetPassworEmaiTest } from './mail/sendResetPasswordEmail.js'
-
-sendResetPassworEmaiTest()
-    .then(() => console.log('Email enviado!'))
-    .catch((err) => console.error('Erro:', err))
