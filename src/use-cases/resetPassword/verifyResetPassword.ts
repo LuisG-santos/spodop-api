493df3b6872ad1553b19e5b4aebbf42c68675d0b
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-07-05 03:27:03 -0300

    feat: implement VerifyResetPasswordCodeUseCase for verifying reset password codes

---
+++ src/use-cases/resetPassword/verifyResetPassword.ts
@@ -0,0 +1,38 @@
+import { AppError } from "../../error/error.js";
+import type { GetForgotPasswordCodeByUserId } from "../../repository/prisma/resetPassword/getForgotPasswordCodeByUserId.js";
+import type { GetUserByEmailRepository } from "../../repository/prisma/user/getUserByEmail.js";
+export class VerifyResetPasswordCodeUseCase {
+  private getForgotPasswordCodeRepository: GetForgotPasswordCodeByUserId;
+  private getUserByEmailRepository: GetUserByEmailRepository;
+  constructor(
+    getForgotPasswordCode: GetForgotPasswordCodeByUserId,
+    getUserByEmail: GetUserByEmailRepository,
+  ) {
+    this.getForgotPasswordCodeRepository = getForgotPasswordCode;
+    this.getUserByEmailRepository = getUserByEmail;
+  }
+  async verifyCode(email: string, code: string): Promise<void> {
+    const userId = await this.getUserByEmailRepository.getEmail(email);
+
+    if (!userId) {
+      throw new AppError("Invalid or expired code", 400);
+    }
+    const resetCode = await this.getForgotPasswordCodeRepository.getCode(
+      userId.id,
+    );
+
+    if (!resetCode) {
+      throw new AppError("Invalid or expired code", 400);
+    }
+
+    const expirationCode = new Date() > resetCode.expiresAt;
+
+    if (expirationCode) {
+      throw new AppError("Invalid or expired code", 400);
+    }
+
+    if (code !== resetCode.code) {
+      throw new AppError("Invalid or expired code", 400);
+    }
+  }
+}
