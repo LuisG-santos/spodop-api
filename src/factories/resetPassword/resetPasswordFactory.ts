8936df22b1eebc6c92d8df825df34241cf91e9aa
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-07-05 03:21:05 -0300

    fix: correct import path for ForgotPasswordUseCase

--- src/factories/resetPassword/resetPasswordFactory.ts
+++ src/factories/resetPassword/resetPasswordFactory.ts
@@ -1,7 +1,7 @@
 import { ForgotPasswordController } from "../../controllers/resetPassword/forgotPassword.js";
 import { UpsertForgotPasswordRepository } from "../../repository/prisma/resetPassword/upsertForgotPassword.js";
 import { GetUserByEmailRepository } from "../../repository/prisma/user/getUserByEmail.js";
-import { ForgotPasswordUseCase } from "../../use-cases/resetPassword/resetPassword.js";
+import { ForgotPasswordUseCase } from "../../use-cases/resetPassword/forgotPassword.js";
 
 export const makeForgotPasswordController = () => {
   const getUserByEmailRepository = new GetUserByEmailRepository();
