63c83951a916f12dbd6543d2ae190f81636b2bf4
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-07-20 15:39:26 -0300

    feat: add Swagger documentation and Postman configuration files

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import swaggerDocument from "./docs/swagger.json" with {type: "json"}
+import swaggerUi from "swagger-ui-express"
 import userRoutes from "./src/routes/user/user.routes.js";
 import authRoutes from "./src/routes/auth/auth.routes.js";
 import ownershipRoutes from "./src/routes/ownership/ownership.route.js"
@@ -17,6 +19,7 @@ if (!process.env.JWT_SECRET_KEY) {
 app.use("/user", userRoutes);
 app.use("/auth", authRoutes);
 app.use("/ownership", ownershipRoutes);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 
 app.listen(3000, () => {
