b9d5c679b019f7ac8924219d538137eeb0ea4188
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-07-23 00:14:27 -0300

    Remove unused Swagger UI Express dependencies from package.json

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import swaggerDocument from "./docs/swagger.json" with {type: "json"}
-import swaggerUi from "swagger-ui-express"
 import userRoutes from "./src/routes/user/user.routes.js";
 import authRoutes from "./src/routes/auth/auth.routes.js";
 import ownershipRoutes from "./src/routes/ownership/ownership.route.js"
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://spodop.com.br",
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -19,8 +20,6 @@ if (!process.env.JWT_SECRET_KEY) {
 app.use("/user", userRoutes);
 app.use("/auth", authRoutes);
 app.use("/ownership", ownershipRoutes);
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
